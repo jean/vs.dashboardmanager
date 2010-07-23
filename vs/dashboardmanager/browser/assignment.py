713092b75d05cb993cdf922d2b5fd8b98db87cc1
Andreas Jung <info@zopyx.com>
2010-07-23 09:09:05 +0000

    added

---
+++ vs/dashboardmanager/browser/assignment.py
@@ -0,0 +1,17 @@
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class AssignmentView(BrowserView):
+
+    template = ViewPageTemplateFile('assignment.pt')
+
+    def getRoles(self):
+        mt = getToolByName(self, 'portal_membership')
+        return  sorted([r for r in mt.getPortalRoles() if r != 'Owner'])
+
+    def __call__(self, *args, **kw):
+        return self.template(*args, **kw)
+
+
