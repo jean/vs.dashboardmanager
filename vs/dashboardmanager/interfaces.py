2d4711194e340667961f292acdd3d252485d8bdb
Andreas Jung <info@zopyx.com>
2010-07-26 06:01:55 +0000

    fixes

--- vs/dashboardmanager/interfaces.py
+++ vs/dashboardmanager/interfaces.py
@@ -1,6 +1,19 @@
 
 from plone.app.portlets.interfaces import IColumn
+from plone.portlets.interfaces import IPortletManager
 
 class IPortletPageColumn(IColumn):
     """Marker interface describing columns on a portlet page
     """
+
+class IPortletPageColumn1(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn2(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn3(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
+
+class IPortletPageColumn4(IPortletManager):
+    """ Portlet manager for vs.dashboardmanager """
