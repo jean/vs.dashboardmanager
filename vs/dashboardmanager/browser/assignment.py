ad129b19835871875eedb763836ce498e54bed19
Andreas Jung <info@zopyx.com>
2010-07-26 08:21:10 +0000

    able to read assignments

--- vs/dashboardmanager/browser/assignment.py
+++ vs/dashboardmanager/browser/assignment.py
@@ -1,7 +1,10 @@
 
+from zope.component import getUtility, getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.portlets.interfaces import IPortletManager, IPortletAssignment, IPortletAssignmentMapping
 
 class AssignmentView(BrowserView):
 
@@ -12,6 +15,16 @@ class AssignmentView(BrowserView):
         return  sorted([r for r in mt.getPortalRoles() if r != 'Owner'])
 
     def push_assignment(self, userid='', roles=[]):
+
+        for i in range(1, 5):
+            manager_name = 'vs.dashboardmanager.column%d' % i
+            print '-'*80
+            print manager_name
+            manager = getUtility(IPortletManager, name=manager_name, context= self.context)
+            mapping = getMultiAdapter((self.context, manager), IPortletAssignmentMapping)
+            for id, assignment in mapping.items():
+                print id, assignment
+
         return 'done'
 
 
