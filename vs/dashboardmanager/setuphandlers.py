04697297ac7901076ac8b1830c20d087393454d6
Andreas Jung <info@zopyx.com>
2010-07-27 11:15:17 +0000

    Plone 4 compatibiliaty

--- vs/dashboardmanager/setuphandlers.py
+++ vs/dashboardmanager/setuphandlers.py
@@ -27,7 +27,6 @@ def setupVarious(context):
     # Here, we are not parsing an XML file, but we use this text file as a
     # flag to check that we actually meant for this import step to be run.
     # The file is found in profiles/default.
-
     if context.readDataFile('vs.dashboardmanager_various.txt') is None:
         return
 
